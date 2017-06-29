9122cd8dbbc736ffb6dc3ee6b306647d0d1e5c52
Lukasz Ludwin <gorli@interia.pl>
2017-06-29 11:00:34 +0100

    second commit

--- components/App.js
+++ components/App.js
@@ -1,5 +1,30 @@
+var GIPHY_API_URL = "http://api.giphy.com";
+var GIPHY_PUB_KEY = "dc6zaTOxFJmzC";
+function httpGet(url_in) {
+	return new Promise(
+		function (resolve, reject) {
+			const request = new XMLHttpRequest();
+			request.onload = function () {
+				if (this.status === 200) {
+					var data = JSON.parse(request.responseText).data; // 4.
+				 	var gif = {  // 5.
+						url: data.fixed_width_downsampled_url,
+						sourceUrl: data.url
+					};
+					resolve(gif)
+				} else {
+					reject(new Error(this.statusText)); 
+				}
+			};
+			request.onerror = function () {
+				reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+			};
+			request.open('GET', url_in);
+			request.send();
+		}
+	);
+}
 App = React.createClass({
-
 	getInitialState() {
 		return {
 			loading: false,
@@ -23,39 +48,12 @@ App = React.createClass({
 		});
 	},
 
-	getGif: function(searchingText, callback) {  // 1.
-		var GIPHY_API_URL = "http://api.giphy.com";
-		var GIPHY_PUB_KEY = "dc6zaTOxFJmzC";
+	getGif: function(searchingText, callback) {  // 1.		
 		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
 	
 		httpGet(url)
-		.then(response => gif)
+		.then(response => callback(response))
 		.catch(error => console.error('The error reason is : ',error));
-
-		function httpGet(url_in) {
-			return new Promise(
-				function (resolve, reject) {
-					const request = new XMLHttpRequest();
-					request.onload = function () {
-						if (this.status === 200) {
-							var data = JSON.parse(request.responseText).data; // 4.
-						 	var gif = {  // 5.
-								url: data.fixed_width_downsampled_url,
-								sourceUrl: data.url
-							};
-							resolve(callback(gif))
-						} else {
-							reject(new Error(this.statusText)); // Dostaliśmy odpowiedź, ale jest to np 404
-						}
-					};
-					request.onerror = function () {
-						reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
-					};
-					request.open('GET', url_in);
-					request.send();
-				}
-			);
-		}
 	},
 
 	render: function() {
